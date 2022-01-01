f39ddfe8dbe78aebd3c38a91d7813e365a2a356b
sonkamblevishal <88578336+sonkamblevishal@users.noreply.github.com>
2022-01-01 16:49:38 +0530

    Add files via upload

---
+++ 21-12-2021/Assignment/character_or_vowel.js
@@ -0,0 +1,41 @@
+// Check wheather the given character is  vowel or not. (Also check for capital characters.
+
+	
+var num=prompt("enter char");
+
+
+if(num=="a" || num=="A")                       
+	{
+
+	alert("it is a Vowel");
+	}
+
+else if (num=="e" || num=="E")
+	{
+	alert("it is a Vowel");
+	}
+
+else if (num=="i" || num=="I")
+	{
+	alert("it is a Vowel");
+	}
+
+
+else if (num=="o" || num=="O")
+	{
+	alert("it is a Vowel");
+	}
+
+
+else if (num=="u" || num=="U")
+	{
+	alert("it is a Vowel");
+	}
+
+else
+	{
+	alert(" it is a consonant")
+	}
+
+
+
---
+++ 21-12-2021/Assignment/minimum_number.js
@@ -0,0 +1,17 @@
+var num1= prompt("Enter 1st number");
+var num2= prompt("Enter 2nd number");
+var num3= prompt("Enter 3rd number");
+
+
+	 if(num1<=num2 && num1<=num3)
+	{
+	alert("Lowest Number is:" +num1);
+	}
+	else if(num2<=num1 && num2<=num3)
+	{
+	alert("Lowest Number is:" +num2);
+	}
+	else
+	{
+	alert("Lowest Number is:" +num3);
+	} 
