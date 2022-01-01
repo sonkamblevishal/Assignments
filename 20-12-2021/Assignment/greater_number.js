68e0a9d6cf46f35bbeb41893a0b4a2d047cf89f2
sonkamblevishal <88578336+sonkamblevishal@users.noreply.github.com>
2022-01-01 16:48:28 +0530

    Add files via upload

---
+++ 20-12-2021/Assignment/greater_number.js
@@ -0,0 +1,14 @@
+// check whether the number is greater than 10
+
+  num=11;
+
+   if(num>10)
+  {
+     console.log("it is greater number");
+    }
+    else
+    {
+   console.log("It is smaller number than 10");
+    }
+
+
---
+++ 20-12-2021/Assignment/highest_number.js
@@ -0,0 +1,22 @@
+// write a program to have 3 variables (all 3 number) and output the highest of three.
+
+
+var a=10;
+var b=20;
+var c=30;
+
+
+     	if(a>=b &&  a>=c)
+	{
+    	console.log(a);
+	}
+
+	else if (b >= a && b >= c)
+	 {
+    	console.log(b);
+	}
+
+	else {
+	    console.log(c);
+	}
+ 
