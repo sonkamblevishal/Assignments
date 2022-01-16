53bb9bd2bd262220a53fbb99ef179208ede0f31c
sonkamblevishal <88578336+sonkamblevishal@users.noreply.github.com>
2022-01-16 15:48:17 +0530

    Add files via upload

---
+++ 12-01-2022/Assignment/index.js
@@ -0,0 +1,15 @@
+var likecount=document.querySelector("#likecount");
+
+var dislikecount=document.querySelector("#dislikecount");
+
+function count1()
+{
+    likecount.value=parseInt(likecount.value) +1;
+}
+
+function count2()
+{
+
+    dislikecount.value=parseInt(likecount.value);
+}
+
