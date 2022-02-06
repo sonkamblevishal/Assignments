dd3dfb7e8e5c228b1ec1c9c4e1a49d5873a1a1d0
sonkamblevishal <88578336+sonkamblevishal@users.noreply.github.com>
2022-02-06 06:22:54 +0530

    Add files via upload

--- 04-02-2022/Assignment/script.js
+++ 04-02-2022/Assignment/script.js
@@ -1,38 +1,49 @@
-const input = document.getElementById("input");
-const grid = document.getElementsByClassName("grid")[0];
+let key = "6lst2APg22QDgYrsyq5O2Zw5pzwQ4bZuoFEU9LEvp60";
+let key2 = "5vGWTTEikbCyuYUU7PhYvRktXrLgrkKzVd12ej54nj4";
 
-input.addEventListener("keydown", function(event){
-  if (event.key === "Enter")
-   loadImg();
-});
-function loadImg() {
-  removeImages();
-  const url =
-    "https://api.unsplash.com/search/photos/?query=" +
-    input.value +
-    "&per_page=30&client_id=f1fEMdfaFfJ_Bqf-t6a9XUAsN72LMYTe6-h0naZsZYQ";
-  fetch(url)
-    .then((response) => {
-      if (response.ok) return response.json();
-      else alert(response.status);
-    })
-    .then((data) => {
-      const imageNodes = [];
-      for (let i = 0; i < data.results.length; i++) {
-        imageNodes[i] = document.createElement("div");
-        imageNodes[i].className = "img";
-        imageNodes[i].style.backgroundImage =
-          "url(" + data.results[i].urls.raw + ")";
-        imageNodes[i].addEventListener("dblclick", function () {
-          window.open(data.results[i].links.download, "_blank");
-        });
-        grid.appendChild(imageNodes[i]);
+let button_reference = document.getElementById("initButton");
+
+let search = document.getElementById("search");
+
+button_reference.addEventListener("click", showImages);
+
+function imageClicked(event) {
+  var img_prev_r = document.getElementById("img_preview");
+  img_prev_r.classList.remove("d-none");
+
+  var img_r = img_prev_r.children[0];
+  img_r.src = event.target.src;
+}
+
+function showImages(element) {
+  element.preventDefault();
+  var container_ref = document.getElementById("gallery_container");
+
+  container_ref.innerText = "";
+
+  fetch(
+    `https://api.unsplash.com/search/photos/?&per_page=30&client_id=${key}&query=${search.value}`
+
+    )
+    .then((x) => x.json())
+    .then((res) => {
+      for (let i = 0; i < res.results.length; i = i + 1) {
+        let single_result = res.results[i];
+        let single_url = single_result.urls.regular;
+        console.log(single_url);
+
+        var img_ref = document.createElement("img");
+        img_ref.src = single_url;
+        img_ref.addEventListener("click", imageClicked);
+        container_ref.appendChild(img_ref);
       }
     });
+  search.value = "";
 }
 
-function removeImages() {
-  grid.innerHTML = "";
+cencel.addEventListener("click", Close);
+
+function Close() {
+  var img_prev_r = document.getElementById("img_preview");
+  img_prev_r.classList.add("d-none");
 }
-
-
